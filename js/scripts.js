9405814930c581b8299a59e4bdcbf027f93b7198
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-02-23 22:42:01 +0300

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,45 @@
 $(document).ready(function() {
-  $("form#pingpong").print(function(event) {
-    var ping = function(number) {
-      return ping
+  $("form#pingpong").submit(function(event) {
+    var answer= parseInt("number").val() {
+      event.preventDefault
     }
   });
 });
+
+
+$(document).ready(function() {
+    $("form#pingpong").submit(function(event) {
+        event.preventDefault();
+      var display = parseInt($("input#number").val());
+      var theAnswer = answer(result);
+      $(".Push").text(theAnswer);
+
+    });
+  });
+
+// business logic
+var answer=function(result){
+  for(i=1; i<=display; i++){
+ // if the number is divisible by 15, write "Ping Pong"
+  if ( i % 15 === 0) {
+    console.log("pingpong");
+    $(".Push").append("<li>"+"pingpong"+"</li>");
+    //return "PingPong";
+   }
+  // if the number is divisible by 3, write "Ping"
+  else if ( i % 3 === 0) {
+    $(".Push").append("<li>"+"ping"+"</>");
+    //return "Ping";
+  }
+    // otherwise, write "po"
+  else if ( i % 5 === 0) {
+    $(".Push").append("<li>"+"pong"+"</>");
+      //return "Pong";
+     }
+  else {
+      console.log("qwert");
+      $(".Push").append("<li>"+i+"</>");
+    //return(i);
+  }
+}
+}
