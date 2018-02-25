9005b8e0802d5566be2ce8b32302824a76b8b329
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-02-25 19:34:45 +0300

    add push id to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+var i, value
 $(document).ready(function() {
     $("#pingpong form").submit(function(event) {
         event.preventDefault();
-      var display = parseInt($("input#number").val());
-      var result = answer(display);
-      $(".Push").text(result);
+      var value = parseInt($("input#number").val());
+      var result = answer(value);
+      $("#Push").text(result);
     });
   });
 
@@ -11,20 +12,20 @@ var answer=function(display){
   for(i=1; i<=display; i++){
 
   if ( i % 15 === 0) {
-    $(".Push").append("<li>"+"pingpong"+"</li>");
+    $("#Push").append("<li>"+"pingpong"+"</li>");
 
    }
 
   else if ( i % 3 === 0){
-    $(".Push").append("<li>"+"ping"+"</>")
+    $("#Push").append("<li>"+"ping"+"</>")
   }
   else if ( i % 5 === 0) {
-    $(".Push").append("<li>"+"pong"+"</>");
+    $("#Push").append("<li>"+"pong"+"</>");
      }
   else {
-  
-      $(".Push").append("<li>"+i+"</>");
+
+      $("#Push").append("<li>"+i+"</>");
 
   }
-}
-}
+ };
+};
