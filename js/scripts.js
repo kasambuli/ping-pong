3edd6dd3088165b5422ca29a9512a067d0141aad
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-02-25 19:32:41 +0300

    remove space from js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,30 @@
 $(document).ready(function() {
-    $("form#pingpong").submit(function(event) {
+    $("#pingpong form").submit(function(event) {
         event.preventDefault();
       var display = parseInt($("input#number").val());
-      var theAnswer = answer(result);
-      $(".Push").text(theAnswer);
-
+      var result = answer(display);
+      $(".Push").text(result);
     });
   });
 
-// business logic
-var answer=function(result){
+var answer=function(display){
   for(i=1; i<=display; i++){
- // if the number is divisible by 15, write "Ping Pong"
+
   if ( i % 15 === 0) {
-    console.log("pingpong");
     $(".Push").append("<li>"+"pingpong"+"</li>");
-    //return "PingPong";
+
    }
-  // if the number is divisible by 3, write "Ping"
-  else if ( i % 3 === 0) {
-    $(".Push").append("<li>"+"ping"+"</>");
-    //return "Ping";
+
+  else if ( i % 3 === 0){
+    $(".Push").append("<li>"+"ping"+"</>")
   }
-    // otherwise, write "po"
   else if ( i % 5 === 0) {
     $(".Push").append("<li>"+"pong"+"</>");
-      //return "Pong";
      }
   else {
-      console.log("qwert");
+  
       $(".Push").append("<li>"+i+"</>");
-    //return(i);
+
   }
 }
 }
